827510ab7b0d46477ffc8cffc7e75148990651ef
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-09-29 21:53:00 +0100

    initial set up complete

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+// express
+
+const express = require("express");
+const app = express();
+
+// database
+const connectDB = require("./db/connect");
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening at ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
