a1de4d341d1c76193a918eaa80d6d5399a437e08
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-10-01 01:28:27 +0100

    setup reusable jwt creation and verification functions

---
+++ controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { createJWT } = require("../utils");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError(
+      `Sorry this Email ${email} already exists`
+    );
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ email, name, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
+const logout = async (req, res) => {
+  res.send("logout user");
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
